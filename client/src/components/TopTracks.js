90ae58284d50d73663437322c1399761a553f826
Thavi <thavit97@gmail.com>
2020-04-15 21:06:30 +0100

    Preview each song

--- client/src/components/TopTracks.js
+++ client/src/components/TopTracks.js
@@ -6,7 +6,7 @@ class TopTracks extends Component {
     super();
     this.state = {
       topTracks: [],
-      focusedSong: '',
+      focusedSong: 0,
     }
 
   }
@@ -23,8 +23,27 @@ class TopTracks extends Component {
     })
   }
 
-  getFocusedSong(){
+  playOrPausePreview(){
+    var song_preview = document.getElementById('song-preview');
+    song_preview.volume = 0.1;
+    if(song_preview.paused){
+      song_preview.play();
+    }else{
+      song_preview.pause();
+    }
+  }
 
+  selectSong(track_index){
+    this.setState({
+      focusedSong: track_index,
+    })
+  }
+
+  autoplaySong(){
+      var song_preview = this.ref.song;
+      song_preview.volume = 0.1;
+      song_preview.play();
+      console.log('hi');
   }
 
   render(){
@@ -33,24 +52,27 @@ class TopTracks extends Component {
       <div className="App">
         <div><b>Top Songs of The Last 6 Months:</b></div>
         <div className="row">
-          <div className="list-group col-sm-3">
+          <div className="list-group col-md-3">
             {this.state.topTracks.map((track) => (
-              <a href="#" className="list-group-item list-group-item-action" key={track.id}>{track.name}</a>
+              <button onClick={() => this.selectSong(this.state.topTracks.indexOf(track))} className="list-group-item list-group-item-action" key={track.id}>{track.name}</button>
             ))}
           </div>
           <div className="col-sm-9">
-            {this.state.topTracks.slice(0,1).map((track) => (
+            {this.state.topTracks.slice(this.state.focusedSong,this.state.focusedSong+1).map((track) => (
               <div key={track.id} className="row">
-                <div className="col-sm-4">
-                  <img src={track.album.images[0].url} style={{ width: 250 }} alt=""/>
+                <div className="col-lg-4">
+                  <img className="img-responsive" src={track.album.images[0].url} style={{ width: 250 }} alt=""/>
                 </div>
-                <div className="col-sm-8">
+                <div className="col-lg-8">
                   <h3>{track.name}</h3>
                   <h5>By: {track.artists[0].name}</h5>
                   <h5>Album: {track.album.name}</h5>
-                  <audio controls>
-                    <source src={track.preview_url} type="audio/mpeg"/>
+                  <audio ref="song" id="song-preview">
+                    <source src={track.preview_url} type="audio/ogg"/>
                   </audio>
+                  <button onClick={() => this.playOrPausePreview()}>
+                      Play/Pause Preview
+                  </button>
                 </div>
               </div>
             ))}
