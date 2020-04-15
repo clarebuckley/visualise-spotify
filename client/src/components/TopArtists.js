a8971bc52133aa40da527c6f94bff2cd4c28c404
Clare Buckley <clarebuckley1@hotmail.com>
2020-04-15 20:31:56 +0100

    loads top 10 artists and displays them, will also get top track by that artist but doesn't display yet

--- client/src/components/TopArtists.js
+++ client/src/components/TopArtists.js
@@ -16,31 +16,66 @@ class TopArtists extends Component {
     getTopArtists = () => {
         this.props.spotifyWebApi.getMyTopArtists({ limit: 10 })
             .then((response) => {
-                console.log(response.items);
-                this.setState({
-                    topArtists: response.items
-                })
+                var artists = response.items;
+                this.getAllArtistsTopTracks(artists)
+                    .then((updatedArtists) => {
+                        this.setState({
+                            topArtists: updatedArtists
+                        })
+                        console.log(this.state.topArtists[0]);
+                    })
+            })
+            .catch((err) => {
+                console.error(err);
             })
     }
-  
+
+    getAllArtistsTopTracks = (artists) => {
+        return new Promise(resolve => {
+            for (let artist of artists) {
+                this.getSingleArtistTopTrack(artist.id)
+                    .then((topTrack) => {
+                        artist.topTrack = topTrack;
+                    })
+            }
+            return resolve(artists);
+        })
+    }
+
+
+
+    getSingleArtistTopTrack = async (artistId) => {
+        return new Promise(resolve => {
+            //need to get rid of "GB" string TODO
+            this.props.spotifyWebApi.getArtistTopTracks(artistId, "GB", { limit: 1 })
+                .then((response) => {
+                    return resolve(response.tracks[0]);
+                }).catch((err) => {
+                    console.error(err);
+                })
+        })
+
+    }
+
 
     render() {
-        
-            return (
-                <div className="TopArtists">
-                    <div className="header">Your top artists</div>
+        return (
+            <div className="TopArtists">
+                <div className="header">Your top artists</div>
+                {this.state && this.state.topArtists &&
                     <div className="resultsContainer">
                         {this.state.topArtists.map((result, index) => (
                             <li className="result">
-                                <img src={result.images[2].url} height="80px" alt="album art" /> 
+                                <img src={result.images[2].url} height="80px" alt="album art" />
                                 <p>{result.name}</p>
+                              
                             </li>
                         ))}
                     </div>
-                </div>
-            );
-        
-
+                }
+                
+            </div>
+        );
     }
 }
 
