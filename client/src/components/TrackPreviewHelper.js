d3a40f8b1a432a37273b243b3651760c01d7d92f
Clare Buckley <clarebuckley1@hotmail.com>
2020-04-16 13:02:03 +0100

    stop other audios from playing when clicking to play audio

--- client/src/components/TrackPreviewHelper.js
+++ client/src/components/TrackPreviewHelper.js
@@ -1,7 +1,13 @@
 export function playOrPausePreview(songPreviewId) {
     var song_preview = document.getElementById(songPreviewId);
+    var audioElements = document.getElementsByTagName("audio");
+
     song_preview.volume = 0.1;
     if (song_preview.paused) {
+        //Stop any other audio elements in the app from playing
+        for (let audio of audioElements) {
+            audio.pause();
+        }
         song_preview.play();
     } else {
         song_preview.pause();
