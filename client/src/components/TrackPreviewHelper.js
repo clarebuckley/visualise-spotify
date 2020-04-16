5a4ce513b8f0a686f02f9d4b1d9fa45d8da7295a
Thavi <thavit97@gmail.com>
2020-04-16 15:44:02 +0100

    Upped the volume a little

--- client/src/components/TrackPreviewHelper.js
+++ client/src/components/TrackPreviewHelper.js
@@ -2,7 +2,7 @@ export function playOrPausePreview(songPreviewId) {
     var song_preview = document.getElementById(songPreviewId);
     var audioElements = document.getElementsByTagName("audio");
 
-    song_preview.volume = 0.1;
+    song_preview.volume = 0.3;
     if (song_preview.paused) {
         //Stop any other audio elements in the app from playing
         for (let audio of audioElements) {
@@ -22,4 +22,3 @@ export function autoplaySong() {
     song_preview.play();
     console.log('hi');
 }
-
