7c4b93516caca2d9f31d2dd4914debc1295024b0
Clare Buckley <clarebuckley1@hotmail.com>
2020-04-16 19:30:41 +0100

    make footer stick to bottom

--- client/src/App.js
+++ client/src/App.js
@@ -51,8 +51,9 @@ class App extends Component {
                             <TopTracks spotifyWebApi={spotifyWebApi} />
                         </Tab>
                     </Tabs>
-                    
-                    <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                    <div className="footer">
+                        <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                    </div>
                 </div >
             )
         }
