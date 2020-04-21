7ad806ed98ef71b64172eea4282a12355babe2d6
Clare Buckley <clarebuckley1@hotmail.com>
2020-04-21 15:16:21 +0100

    use correct api

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 import './Login.css';
 
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            environment: null
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            environment: this.getEnvironment()
+        })
+    }
+
+    getEnvironment = () => {
+        var url = window.location.href;
+        if (url.includes("localhost")) {
+            return "http://localhost:8888/login"
+        } else {
+            return "https://heroku-auth-server.herokuapp.com/login"
+        }
+    }
     render() {
+        if (!this.state.environment) { return <p>Loading...</p> }
         return (
             <div className="Login">
-                <a className="loginLink" href="https://heroku-auth-server.herokuapp.com/login">
+                <a className="loginLink" href={this.state.environment}>
                     Login With Spotify
                 </a>
             </div>
