3860cb6c5484c7c51aa9b5126ee51d385192bd93
Thavi <thavit97@gmail.com>
2020-05-01 17:45:01 +0100

    Display message depending on popularity rating

--- client/src/components/top-tracks/TopTracksIndividualSong.js
+++ client/src/components/top-tracks/TopTracksIndividualSong.js
@@ -8,6 +8,36 @@ import {Spring} from 'react-spring/renderprops';
  * Also gets the popularity of that track and displays it as a pie chart over the album art.
  * */
 class TopTracksIndividualSong extends Component {
+  constructor(){
+    super();
+    this.state = {
+      averagePopularityText: "",
+    }
+  }
+
+    calculateAveragePopularity = () =>{
+      var sumOfPopularities = 0;
+      for (var i = 0; i < this.props.popularityChart.datasets[0].data.length; i++) {
+        sumOfPopularities += this.props.popularityChart.datasets[0].data[i];
+      }
+      var averagePopularity = sumOfPopularities/this.props.numberOfSongs;
+      return averagePopularity
+    }
+
+    generateTextForAveragePopularity = (averagePopularity) =>{
+      var averagePopularityText = "";
+      if (averagePopularity<25) {
+        averagePopularityText = "Hey, not all popular music is bad! Give it a try sometime."
+      }else if (averagePopularity>=25 && averagePopularity<50) {
+        averagePopularityText = "You mainly listen to less popular songs, you indiehead."
+      }else if (averagePopularity>=50 && averagePopularity<75) {
+        averagePopularityText = "This means you like to listen to well known tracks but you also like some underground tunes."
+      }else if (averagePopularity>=75) {
+        averagePopularityText = "You have no individual taste."
+      }
+      return averagePopularityText
+    }
+
     render() {
         return (
           <div className="col-lg-8">
@@ -49,9 +79,8 @@ class TopTracksIndividualSong extends Component {
                 </Spring>
               </div>
             ))}
-            <div className="margin-bottom col-lg-12">
-              <div className="col-lg-10 offset-lg-1">
-                <Bar
+            <div className="margin-bottom col-lg-10 offset-lg-1">
+              <Bar
                 data={this.props.popularityChart}
                 options={{
                   scales: {
@@ -89,7 +118,14 @@ class TopTracksIndividualSong extends Component {
                     }
                   }
                 }}
-                />
+              />
+            </div>
+            <div className="col-lg-12 popularity-text-container">
+              <div>
+                The average popularity score for these songs is {this.calculateAveragePopularity()}/100!
+              </div>
+              <div>
+                {this.generateTextForAveragePopularity(this.calculateAveragePopularity())}
               </div>
             </div>
           </div>
